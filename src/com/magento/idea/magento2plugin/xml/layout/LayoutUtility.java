8b35c5fc93a0530f2969dae1a5de1ca5e1914952
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-11-22 20:01:31 +0200

    Added reference/completion for block, move, remove, update layout tags

---
+++ src/com/magento/idea/magento2plugin/xml/layout/LayoutUtility.java
@@ -0,0 +1,50 @@
+package com.magento.idea.magento2plugin.xml.layout;
+
+import com.intellij.ide.highlighter.XmlFileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElementResolveResult;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.xml.XmlFile;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by dkvashnin on 11/22/15.
+ */
+public class LayoutUtility {
+    public static boolean isLayoutFile(VirtualFile virtualFile) {
+        VirtualFile parent = virtualFile.getParent();
+        return virtualFile.getFileType() == XmlFileType.INSTANCE && parent.isDirectory() && parent.getName().endsWith("layout");
+    }
+
+    public static List<XmlFile> getLayoutFiles(Project project, @Nullable String fileName) {
+        List<XmlFile> results = new ArrayList<XmlFile>();
+        Collection<VirtualFile> xmlFiles = FilenameIndex.getAllFilesByExt(project, "xml");
+
+        PsiManager psiManager = PsiManager.getInstance(project);
+        for (VirtualFile xmlFile: xmlFiles) {
+            if (LayoutUtility.isLayoutFile(xmlFile)) {
+                if (fileName != null && !xmlFile.getNameWithoutExtension().equals(fileName)) {
+                    continue;
+                }
+
+                PsiFile file = psiManager.findFile(xmlFile);
+                if (file != null) {
+                    results.add((XmlFile)file);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public static List<XmlFile> getLayoutFiles(Project project) {
+        return getLayoutFiles(project, null);
+    }
+}
--- src/com/magento/idea/magento2plugin/xml/layout/completion/LayoutCompletionContributor.java
+++ src/com/magento/idea/magento2plugin/xml/layout/completion/LayoutCompletionContributor.java
@@ -2,20 +2,26 @@ package com.magento.idea.magento2plugin.xml.layout.completion;
 
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.project.Project;
+import com.intellij.patterns.XmlPatterns;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.PlatformIcons;
 import com.intellij.util.ProcessingContext;
 import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.xml.util.XmlIconProvider;
 import com.jetbrains.php.PhpIcons;
 import com.magento.idea.magento2plugin.xml.XmlHelperUtility;
 import com.magento.idea.magento2plugin.xml.completion.ClassCompletionProvider;
 import com.magento.idea.magento2plugin.xml.completion.CompletionProviderI;
 import com.magento.idea.magento2plugin.xml.completion.VirtualTypeCompletionProvider;
+import com.magento.idea.magento2plugin.xml.layout.LayoutUtility;
 import com.magento.idea.magento2plugin.xml.layout.index.AbstractComponentNameFileBasedIndex;
 import com.magento.idea.magento2plugin.xml.layout.index.BlockFileBasedIndex;
 import com.magento.idea.magento2plugin.xml.layout.index.ContainerFileBasedIndex;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * Created by dkvashnin on 11/18/15.
@@ -59,12 +65,40 @@ public class LayoutCompletionContributor extends CompletionContributor {
 
         extend(
             CompletionType.BASIC,
-            XmlHelperUtility.getTagAttributePattern("referenceBlock", "name"),
+            XmlPatterns.or(
+                XmlHelperUtility.getTagAttributePattern("referenceBlock", "name"),
+                XmlHelperUtility.getTagAttributePattern("block", "before"),
+                XmlHelperUtility.getTagAttributePattern("block", "after"),
+                XmlHelperUtility.getTagAttributePattern("remove", "name"),
+                XmlHelperUtility.getTagAttributePattern("move", "element"),
+                XmlHelperUtility.getTagAttributePattern("move", "destination"),
+                XmlHelperUtility.getTagAttributePattern("move", "before"),
+                XmlHelperUtility.getTagAttributePattern("move", "after")
+            ),
             new CompletionProvider<CompletionParameters>() {
                 @Override
                 protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet) {
                     PsiElement psiElement = completionParameters.getOriginalPosition();
                     completionResultSet.addAllElements(blockCompletionProvider.collectCompletionResult(psiElement));
+                }
+            }
+        );
+
+        extend(
+            CompletionType.BASIC,
+            XmlHelperUtility.getTagAttributePattern("update", "handle"),
+            new CompletionProvider<CompletionParameters>() {
+                @Override
+                protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet completionResultSet) {
+                    PsiElement psiElement = completionParameters.getOriginalPosition();
+                    Project project = psiElement.getProject();
+
+                    completionResultSet.addAllElements(
+                        LayoutUtility.getLayoutFiles(project)
+                            .stream()
+                            .map(e -> LookupElementBuilder.create(e.getVirtualFile().getNameWithoutExtension()))
+                            .collect(Collectors.toList())
+                    );
                 }
             }
         );
