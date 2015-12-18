e4ab865812f5b03f99341a683c83904b08d263ad
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-12-18 16:05:38 -0600

    Added ObjectManager usage inspection in module scope
    (cherry picked from commit c38bb88)

--- src/com/magento/idea/magento2plugin/php/util/MagentoTypes.java
+++ src/com/magento/idea/magento2plugin/php/util/MagentoTypes.java
@@ -6,4 +6,5 @@ package com.magento.idea.magento2plugin.php.util;
 public class MagentoTypes {
     public static final String BLOCK_TYPE = "Magento\\Framework\\View\\Element\\BlockInterface";
     public static final String OBSERVER_TYPE = "Magento\\Framework\\Event\\ObserverInterface";
+    public static final String OBJECT_MANAGER_TYPE = "Magento\\Framework\\ObjectManagerInterface";
 }
