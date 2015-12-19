13fb080f9bba3e4d28dd9b763cbf6d55bc02e372
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-12-19 12:21:02 +0200

    Improved event references for Observeres resolution

--- src/com/magento/idea/magento2plugin/php/util/MagentoTypes.java
+++ src/com/magento/idea/magento2plugin/php/util/MagentoTypes.java
@@ -7,4 +7,5 @@ public class MagentoTypes {
     public static final String BLOCK_TYPE = "Magento\\Framework\\View\\Element\\BlockInterface";
     public static final String OBSERVER_TYPE = "Magento\\Framework\\Event\\ObserverInterface";
     public static final String OBJECT_MANAGER_TYPE = "Magento\\Framework\\ObjectManagerInterface";
+    public static final String EVENT_MANAGER_TYPE = "Magento\\Framework\\Event\\ManagerInterface";
 }
