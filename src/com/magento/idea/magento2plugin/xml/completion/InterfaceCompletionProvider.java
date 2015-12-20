aab4495a31317e12bbf637e07f285e1544317730
Michail Slabko <mslabko@ebay.com>
2015-12-20 09:32:19 -0600

    Add autocompletion for webapi services
    (cherry picked from commit 97e43bd)

---
+++ src/com/magento/idea/magento2plugin/xml/completion/InterfaceCompletionProvider.java
@@ -0,0 +1,53 @@
+package com.magento.idea.magento2plugin.xml.completion;
+
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.php.PhpIcons;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.magento.idea.magento2plugin.util.PsiContextMatcherI;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by mslabko on 18.12.2015.
+ */
+public class InterfaceCompletionProvider implements CompletionProviderI {
+    public final static CompletionProviderI INSTANCE = new InterfaceCompletionProvider();
+
+    @Override
+    public List<LookupElement> collectCompletionResult(PsiElement psiElement, @Nullable PsiContextMatcherI context) {
+        List<LookupElement> result = new ArrayList<>();
+        PhpIndex phpIndex = PhpIndex.getInstance(psiElement.getProject());
+
+        Collection<String> interfaceNames = phpIndex.getAllInterfaceNames();
+
+        for (String interfaceName: interfaceNames) {
+            Collection<PhpClass> classesByName = phpIndex.getInterfacesByName(interfaceName);
+            for (PhpClass phpClass: classesByName) {
+                if (context != null && !context.match(phpClass)) {
+                    continue;
+                }
+
+                String classFqn = phpClass.getPresentableFQN();
+
+                result.add(
+                    LookupElementBuilder
+                        .create(classFqn)
+                        .withIcon(PhpIcons.CLASS_ICON)
+                );
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<LookupElement> collectCompletionResult(PsiElement psiElement) {
+        return collectCompletionResult(psiElement, null);
+    }
+}
