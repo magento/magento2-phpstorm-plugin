ebc2a17af7326f8e6f504b019ad1ec7b84e686d4
Vitaliy Boyko <v.boyko@atwix.com>
2020-03-18 21:45:05 +0200

    Fixed copyrights

--- src/com/magento/idea/magento2plugin/actions/generation/data/MagentoPluginMethodData.java
+++ src/com/magento/idea/magento2plugin/actions/generation/data/MagentoPluginMethodData.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 package com.magento.idea.magento2plugin.actions.generation.data;
 
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
