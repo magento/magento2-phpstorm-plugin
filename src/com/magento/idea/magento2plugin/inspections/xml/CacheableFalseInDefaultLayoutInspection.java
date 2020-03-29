49559bd512878ee958168448cf52ff312d2b4926
Vitaliy Boyko <v.boyko@atwix.com>
2020-03-29 19:06:50 +0300

    Adjusting a code

--- src/com/magento/idea/magento2plugin/inspections/xml/CacheableFalseInDefaultLayoutInspection.java
+++ src/com/magento/idea/magento2plugin/inspections/xml/CacheableFalseInDefaultLayoutInspection.java
@@ -11,27 +11,25 @@ import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.XmlElementVisitor;
 import com.intellij.psi.xml.XmlAttribute;
 import com.magento.idea.magento2plugin.inspections.xml.fix.XmlRemoveCacheableAttributeQuickFix;
+import com.magento.idea.magento2plugin.magento.files.LayoutXml;
 import org.jetbrains.annotations.NotNull;
 
-
-public class CacheDisableInspection extends XmlSuppressableInspectionTool {
+public class CacheableFalseInDefaultLayoutInspection extends XmlSuppressableInspectionTool {
     @NotNull
     @Override
     public PsiElementVisitor buildVisitor(final @NotNull ProblemsHolder holder, final boolean isOnTheFly) {
         return new XmlElementVisitor() {
-            private static final String CacheDisableProblemDescription = "Cacheable false will make whole site uncacheable";
-            private static final String DefaultFileName = "default.xml";
-            private static final String CacheableAttributeName = "cacheable";
-            private static final String CacheableAttributeValue = "false";
+            private static final String CacheDisableProblemDescription = "Cacheable false attribute on the default layout will disable cache site-wide";
             @Override
             public void visitXmlAttribute(XmlAttribute attribute) {
                 String fileName = holder.getFile().getName();
-                if (!fileName.equals(DefaultFileName)) return;
+                if (!fileName.equals(LayoutXml.DefaultFileName)) return;
                 final String text = attribute.getValue();
                 final String attributeName = attribute.getName();
-                if (!attributeName.equals(CacheableAttributeName)) return;
+                if (!attributeName.equals(LayoutXml.CacheableAttributeName)) return;
+                if (!attribute.getParent().getName().equals(LayoutXml.BlockAttributeTagName)) return;
                 if (text == null) return;
-                if (text.equals(CacheableAttributeValue)) {
+                if (text.equals(LayoutXml.CacheableAttributeFalseValue)) {
                     holder.registerProblem(attribute, CacheDisableProblemDescription,
                             ProblemHighlightType.WARNING,
                             new XmlRemoveCacheableAttributeQuickFix());
