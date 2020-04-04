1506f60b6c7d3d6d2e2ef9e7bff6037925f3d26e
bohar <bohar@smile.fr>
2020-04-04 03:23:38 +0300

    [XmlCompletion] #91 - Added php class completion for source_model tag in system.xml and source_model parameter tag attribute in widget.xml

---
+++ src/com/magento/idea/magento2plugin/magento/files/ModuleSystemXml.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+package com.magento.idea.magento2plugin.magento.files;
+
+public class ModuleSystemXml {
+    public static final String FILE_NAME = "system.xml";
+    public static final String FIELD_ELEMENT_NAME = "field";
+    public static final String SOURCE_MODEL_ELEMENT_NAME = "source_model";
+}
