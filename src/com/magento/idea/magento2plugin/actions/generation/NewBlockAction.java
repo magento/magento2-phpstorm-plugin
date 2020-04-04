307eafb01f3860a953878675cdea46ac8de54202
Vitaliy Boyko <v.boyko@atwix.com>
2020-04-04 16:51:19 +0300

    Static fixes

--- src/com/magento/idea/magento2plugin/actions/generation/NewBlockAction.java
+++ src/com/magento/idea/magento2plugin/actions/generation/NewBlockAction.java
@@ -7,16 +7,9 @@ package com.magento.idea.magento2plugin.actions.generation;
 import com.intellij.ide.IdeView;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.jetbrains.php.lang.psi.PhpFile;
-import com.jetbrains.php.lang.psi.elements.Method;
 import com.magento.idea.magento2plugin.MagentoIcons;
 import com.magento.idea.magento2plugin.actions.generation.dialog.NewBlockDialog;
-import com.magento.idea.magento2plugin.actions.generation.generator.code.PluginMethodsGenerator;
-import com.magento.idea.magento2plugin.magento.files.RegistrationPhp;
-import com.magento.idea.magento2plugin.project.Settings;
 import org.jetbrains.annotations.NotNull;
 
 public class NewBlockAction extends AnAction {
@@ -27,26 +20,20 @@ public class NewBlockAction extends AnAction {
         super(ACTION_NAME, ACTION_DESCRIPTION, MagentoIcons.MODULE);
     }
 
-    /**
-     * Triggers when new item is clicked
-     */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         DataContext dataContext = e.getDataContext();
         IdeView view = LangDataKeys.IDE_VIEW.getData(dataContext);
-
         if (view == null) {
             return;
         }
 
         Project project = CommonDataKeys.PROJECT.getData(dataContext);
-
         if (project == null) {
             return;
         }
 
         PsiDirectory directory = view.getOrChooseDirectory();
-
         if (directory == null) {
             return;
         }
