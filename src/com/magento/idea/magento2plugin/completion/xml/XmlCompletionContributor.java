7085982f75aaf8df4700e4206815aa835a89af28
bohar <bohar@smile.fr>
2020-04-07 18:50:47 +0300

    [xmlCompletion] #92 - Fixed wrong completion applying by file name

--- src/com/magento/idea/magento2plugin/completion/xml/XmlCompletionContributor.java
+++ src/com/magento/idea/magento2plugin/completion/xml/XmlCompletionContributor.java
@@ -107,7 +107,7 @@ public class XmlCompletionContributor extends CompletionContributor {
         extend(CompletionType.BASIC, psiElement(XmlTokenType.XML_DATA_CHARACTERS)
             .inside(XmlPatterns.xmlTag().withName(ModuleSystemXml.SOURCE_MODEL_ELEMENT_NAME)
                 .withParent(XmlPatterns.xmlTag().withName(ModuleSystemXml.FIELD_ELEMENT_NAME))
-            ).inFile(xmlFile().withName(string().endsWith(ModuleSystemXml.FILE_NAME))),
+            ).inFile(xmlFile().withName(string().matches(ModuleSystemXml.FILE_NAME))),
             new PhpClassCompletionProvider()
         );
 
@@ -116,7 +116,7 @@ public class XmlCompletionContributor extends CompletionContributor {
             .inside(XmlPatterns.xmlAttribute().withName(ModuleWidgetXml.ATTRIBUTE_SOURCE_MODEL_NAME)
                 .withParent(XmlPatterns.xmlTag().withName(ModuleWidgetXml.TAG_PARAMETER_NAME).
                     withParent(XmlPatterns.xmlTag().withName(ModuleWidgetXml.TAG_PARAMETERS_NAME)))
-            ).inFile(xmlFile().withName(string().endsWith(ModuleWidgetXml.FILE_NAME))),
+            ).inFile(xmlFile().withName(string().matches(ModuleWidgetXml.FILE_NAME))),
                 new PhpClassCompletionProvider()
         );
 
