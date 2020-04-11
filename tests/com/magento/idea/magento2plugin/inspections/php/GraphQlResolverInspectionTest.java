ff26c2a965322f8f8ac10d5d93c6282f75aac2c6
Shankar Konar <konar.shankar2013@gmail.com>
2020-04-11 10:40:56 +0530

    Test added for graphql resolver inspection

---
+++ tests/com/magento/idea/magento2plugin/inspections/php/GraphQlResolverInspectionTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+package com.magento.idea.magento2plugin.inspections.php;
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import java.io.File;
+
+public class GraphQlResolverInspectionTest extends BasePlatformTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        myFixture.enableInspections(GraphQlResolverInspection.class);
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return new File("testData/inspections/php/"
+                + getClass().getSimpleName().replace("Test", "")).getAbsolutePath();
+    }
+
+    @Override
+    protected boolean isWriteActionRequired() {
+        return false;
+    }
+
+    public void testWithInvalidResolverInterface() throws Exception {
+        myFixture.addFileToProject(getTestDataPath() + "/" + "schema.graphqls", "type Query {" +
+                "InvalidResolverTest: InvalidResolver @resolver(class: \"\\\\Magento\\\\Test\\\\InvalidResolverTest\")" +
+                "}");
+        myFixture.configureByFile(getTestName(true) + "/" + "InvalidResolverTest.php");
+        myFixture.checkHighlighting();
+    }
+
+    public void testWithValidResolverInterface() throws Exception {
+        myFixture.addFileToProject(getTestDataPath() + "/" + "schema.graphqls", "type mutation {" +
+                "    ValidResolverTest(): ValidResolver @resolver(class: \"\\\\Magento\\\\Test\\\\ValidResolverTest\")" +
+                "}");
+        myFixture.configureByFile(getTestName(true) + "/" + "ValidResolverTest.php");
+        myFixture.checkHighlighting();
+    }
+}
