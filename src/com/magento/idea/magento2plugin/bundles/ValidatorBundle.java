0d7bdc2de4c6caa9b82e0ad127a15c57da36c39e
eduard13 <e.chitoraga@atwix.com>
2020-04-12 15:22:21 +0300

    Refactoring the bundle messaging

---
+++ src/com/magento/idea/magento2plugin/bundles/ValidatorBundle.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.bundles;
+
+public class ValidatorBundle extends AbstractBundle {
+    protected final String BUNDLE_NAME = "magento2.validation";
+
+    public String getBundleName() {
+        return BUNDLE_NAME;
+    }
+}
