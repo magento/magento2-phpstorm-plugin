900fe5d96746388736ddd1d26ca085e401746b44
Sergiy Zhovnir <s.zhovnir@atwix.com>
2020-04-27 18:07:11 +0300

    #issue-198 Covered module view model class generator with test

---
+++ tests/com/magento/idea/magento2plugin/actions/generation/generator/ModuleViewModelClassGeneratorTest.java
@@ -0,0 +1,33 @@
+package com.magento.idea.magento2plugin.actions.generation.generator;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.magento.idea.magento2plugin.actions.generation.data.ViewModelFileData;
+
+public class ModuleViewModelClassGeneratorTest extends BaseGeneratorTestCase {
+
+    public void testGenerateViewModelFile() {
+        Project project = myFixture.getProject();
+
+        ViewModelFileData viewModelData = new ViewModelFileData(
+                "ViewModel",
+                "TestViewModel",
+                "Foo_Bar",
+                "Foo\\Bar\\ViewModel"
+        );
+        ModuleViewModelClassGenerator moduleViewModelClassGenerator = new ModuleViewModelClassGenerator(
+                viewModelData,
+                project
+        );
+        PsiFile viewModelFile = moduleViewModelClassGenerator.generate("test");
+
+        String filePath = this.getFixturePath("TestViewModel.php");
+        PsiFile expectedFile = myFixture.configureByFile(filePath);
+
+        assertGeneratedFileIsCorrect(
+                expectedFile,
+                "src/app/code/Foo/Bar/ViewModel",
+                viewModelFile
+        );
+    }
+}
