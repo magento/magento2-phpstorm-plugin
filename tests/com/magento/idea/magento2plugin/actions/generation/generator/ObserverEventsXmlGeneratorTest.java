de390eeb72e90f128324490556f390d2cd986043
Sergiy Zhovnir <s.zhovnir@atwix.com>
2020-04-28 16:54:20 +0300

    #issue-200 Covered observer events.xml generator

---
+++ tests/com/magento/idea/magento2plugin/actions/generation/generator/ObserverEventsXmlGeneratorTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+package com.magento.idea.magento2plugin.actions.generation.generator;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.magento.idea.magento2plugin.actions.generation.data.ObserverEventsXmlData;
+import com.magento.idea.magento2plugin.magento.files.ModuleEventsXml;
+import com.magento.idea.magento2plugin.magento.packages.File;
+import com.magento.idea.magento2plugin.magento.packages.Package;
+
+public class ObserverEventsXmlGeneratorTest extends BaseGeneratorTestCase {
+    private static final String moduleName = "Foo_Bar";
+    private static final String moduleDir = "src/app/code/Foo/Bar/";
+
+    public void testGenerateEventsXmlInBaseAreaFile() {
+        String filePath = this.getFixturePath(ModuleEventsXml.FILE_NAME);
+        PsiFile expectedFile = myFixture.configureByFile(filePath);
+        Project project = myFixture.getProject();
+        String area = Package.Areas.base.toString();
+        PsiFile eventsXml = addEventToEventsXml(
+                project,
+                area,
+                "test_event",
+                "test_observer",
+                "Foo\\Bar\\Observer\\Test\\TestEventObserver"
+        );
+
+        assertGeneratedFileIsCorrect(
+                expectedFile,
+                moduleDir + Package.MODULE_BASE_AREA_DIR,
+                eventsXml
+        );
+    }
+
+    public void testGenerateEventsXmlInAdminhtmlAreaFile() {
+        String filePath = this.getFixturePath(ModuleEventsXml.FILE_NAME);
+        PsiFile expectedFile = myFixture.configureByFile(filePath);
+        Project project = myFixture.getProject();
+        String area = Package.Areas.adminhtml.toString();
+        PsiFile eventsXml = addEventToEventsXml(
+                project,
+                area,
+                "test_event",
+                "test_observer",
+                "Foo\\Bar\\Observer\\Test\\TestEventObserver"
+        );
+
+        assertGeneratedFileIsCorrect(
+                expectedFile,
+                moduleDir + Package.MODULE_BASE_AREA_DIR + File.separator + area,
+                eventsXml
+        );
+    }
+
+    public void testAddTwoObserversToOneEventsXml() {
+        String filePath = this.getFixturePath(ModuleEventsXml.FILE_NAME);
+        PsiFile expectedFile = myFixture.configureByFile(filePath);
+        Project project = myFixture.getProject();
+        addEventToEventsXml(
+                project,
+                Package.Areas.frontend.toString(),
+                "test_event",
+                "test_observer",
+                "Foo\\Bar\\Observer\\Test\\TestEventObserver"
+        );
+        PsiFile eventsXml = addEventToEventsXml(
+                project,
+                Package.Areas.frontend.toString(),
+                "test_event_2",
+                "test_observer_2",
+                "Foo\\Bar\\Observer\\Test\\TestEventObserverTwo"
+        );
+
+        assertGeneratedFileIsCorrect(
+                expectedFile,
+                moduleDir + Package.MODULE_BASE_AREA_DIR + File.separator + Package.Areas.frontend.toString(),
+                eventsXml
+        );
+    }
+
+    private PsiFile addEventToEventsXml(
+            Project project,
+            String area,
+            String eventName,
+            String observerName,
+            String observerClassFqn
+    ) {
+        ObserverEventsXmlData observerEventsXmlData = new ObserverEventsXmlData(
+                area,
+                moduleName,
+                eventName,
+                observerName,
+                observerClassFqn
+        );
+        ObserverEventsXmlGenerator observerEventsXmlGenerator = new ObserverEventsXmlGenerator(
+                observerEventsXmlData,
+                project
+        );
+
+        return  observerEventsXmlGenerator.generate("test");
+    }
+}
