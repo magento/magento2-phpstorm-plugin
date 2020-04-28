3f21273bcda1b601ca1ecf196e12441ae8680118
Vitaliy Boyko <v.boyko@atwix.com>
2020-04-28 13:10:06 +0300

    Adjusted tests and fixed bugs

--- tests/com/magento/idea/magento2plugin/completion/xml/RequireJsCompletionRegistrarTest.java
+++ tests/com/magento/idea/magento2plugin/completion/xml/RequireJsCompletionRegistrarTest.java
@@ -6,13 +6,41 @@ package com.magento.idea.magento2plugin.completion.xml;
 
 public class RequireJsCompletionRegistrarTest extends CompletionXmlFixtureTestCase {
 
-    public void testMappedComponentMustHaveCompletion() {
+    public void testMappedComponentAttributeMustHaveCompletion() {
         String filePath = this.getFixturePath("test_form.xml");
         myFixture.copyFileToProject(filePath);
 
         assertCompletionContains(filePath, new String[] {
             "testFile",
             "testFile2"
+        });
+    }
+
+    public void testMappedComponentItemAttributeMustHaveCompletion() {
+        String filePath = this.getFixturePath("test_form.xml");
+        myFixture.copyFileToProject(filePath);
+
+        assertCompletionContains(filePath, new String[] {
+            "testFile",
+            "testFile2"
+        });
+    }
+
+    public void testFileComponentAttributeMustHaveCompletion() {
+        String filePath = this.getFixturePath("test_form.xml");
+        myFixture.copyFileToProject(filePath);
+
+        assertCompletionContains(filePath, new String[] {
+            "Foo_Bar/js/file"
+        });
+    }
+
+    public void testFileComponentItemAttributeMustHaveCompletion() {
+        String filePath = this.getFixturePath("test_form.xml");
+        myFixture.copyFileToProject(filePath);
+
+        assertCompletionContains(filePath, new String[] {
+            "Foo_Bar/js/file"
         });
     }
 }
