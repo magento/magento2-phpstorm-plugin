24f4770d34652e0c9588bd4bfeb6780f5df36b67
Vitaliy Boyko <v.boyko@atwix.com>
2020-05-05 14:56:24 +0300

    Added enums

---
+++ src/com/magento/idea/magento2plugin/magento/packages/Areas.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.magento.packages;
+
+public enum Areas {
+    base,
+    adminhtml,
+    frontend,
+    crontab,
+    webapi_rest,
+    webapi_soap,
+    graphql
+}
