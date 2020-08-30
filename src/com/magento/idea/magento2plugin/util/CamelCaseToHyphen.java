e15f84290c354fe7eecc59836661075fc9d48203
Dylan Ngo <it.tinhngo@gmail.com>
2020-08-30 16:54:34 +0700

    [#263]_update code fix missing first letter

--- src/com/magento/idea/magento2plugin/util/CamelCaseToHyphen.java
+++ src/com/magento/idea/magento2plugin/util/CamelCaseToHyphen.java
@@ -2,16 +2,40 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CamelCaseToHyphen {
     private static CamelCaseToHyphen INSTANCE = null;
+
+    /**
+     * getInstance.
+     * @return CamelCaseToHyphen
+     */
     public static CamelCaseToHyphen getInstance() {
-        if (null == INSTANCE) INSTANCE = new CamelCaseToHyphen();
+        if (null == INSTANCE) {
+            INSTANCE = new CamelCaseToHyphen();
+        }
         return INSTANCE;
     }
 
+    /**
+     * convert.
+     * @param string the origin string.
+     * @return string
+     */
     public String convert(String string) {
-        return string.toLowerCase();
+        String regex = "(?=[A-Z][a-z])";
+        String subst = "-";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(string);
+        String result = matcher.replaceAll(subst).toLowerCase();
+        if (result.charAt(0) == '-') {
+            return result.substring(1);
+        }
+        return result;
     }
 }
