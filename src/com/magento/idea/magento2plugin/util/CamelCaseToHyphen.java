df51f7f9c3a33cd43aefeed1055e323e184d4984
Vitaliy Boyko <v.boyko@atwix.com>
2020-08-30 15:55:19 +0300

    263: static fixes

--- src/com/magento/idea/magento2plugin/util/CamelCaseToHyphen.java
+++ src/com/magento/idea/magento2plugin/util/CamelCaseToHyphen.java
@@ -5,35 +5,45 @@
 
 package com.magento.idea.magento2plugin.util;
 
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@SuppressWarnings({
+        "PMD.ClassNamingConventions",
+        "PMD.NonThreadSafeSingleton"
+})
 public class CamelCaseToHyphen {
-    private static CamelCaseToHyphen INSTANCE = null;
+    private static CamelCaseToHyphen instance;
 
     /**
-     * getInstance.
+     * Get Singleton.
+     *
      * @return CamelCaseToHyphen
      */
     public static CamelCaseToHyphen getInstance() {
-        if (null == INSTANCE) {
-            INSTANCE = new CamelCaseToHyphen();
+        if (null == instance) {
+            instance = new CamelCaseToHyphen();
         }
-        return INSTANCE;
+        return instance;
     }
 
     /**
-     * convert.
+     * Converts camel case to hyphen.
+     * e.g. "TestString" -> "test-string"
+     *
      * @param string the origin string.
      * @return string
      */
-    public String convert(String string) {
-        String regex = "(?=[A-Z][a-z])";
-        String subst = "-";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(string);
-        String result = matcher.replaceAll(subst).toLowerCase();
-        if (result.charAt(0) == '-') {
+    public String convert(final String string) {
+        final String regex = "(?=[A-Z][a-z])";
+        final String subst = "-";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(string);
+        final String result =
+                matcher.replaceAll(subst).toLowerCase(new Locale("en","EN"));
+        final char hyphenSeparator = '-';
+        if (result.charAt(0) == hyphenSeparator) {
             return result.substring(1);
         }
         return result;
