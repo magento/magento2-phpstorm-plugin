c9f3c2be37e1ef462a21800925fc9750ed9ca35c
Vitaliy Boyko <v.boyko@atwix.com>
2020-09-10 20:05:33 +0300

    267: removed redundant file

--- tests/com/magento/idea/magento2plugin/actions/generation/generator/ModuleXmlGeneratorHaveSetupVersionTest.java
+++ tests/com/magento/idea/magento2plugin/actions/generation/generator/ModuleXmlGeneratorHaveSetupVersionTest.java
@@ -5,7 +5,6 @@ import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.magento.idea.magento2plugin.actions.generation.data.ModuleXmlData;
 import com.magento.idea.magento2plugin.magento.files.ModuleXml;
-import com.magento.idea.magento2plugin.magento.files.ModuleXmlHaveSetupVersion;
 import com.magento.idea.magento2plugin.magento.packages.File;
 import com.magento.idea.magento2plugin.magento.packages.Package;
 
@@ -15,7 +14,7 @@ public class ModuleXmlGeneratorHaveSetupVersionTest extends BaseGeneratorTestCas
      * Test checks whether module.xml is generated correctly.
      */
     public void testGenerateModuleFile() {
-        final String filePath = this.getFixturePath(ModuleXmlHaveSetupVersion.FILE_NAME);
+        final String filePath = this.getFixturePath(ModuleXml.FILE_NAME);
         final PsiFile expectedFile = myFixture.configureByFile(filePath);
         final PsiDirectory projectDir = getProjectDirectory();
 
