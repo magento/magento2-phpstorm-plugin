eeccce0bdfe8e72957a362e982f5517d55d19602
Dylan Ngo <it.tinhngo@gmail.com>
2020-10-27 18:18:09 +0700

    #305_write unit test for UIComponent completion

--- tests/com/magento/idea/magento2plugin/completion/xml/XLayoutContainerCompletionRegistrarTest.java
+++ tests/com/magento/idea/magento2plugin/completion/xml/XLayoutContainerCompletionRegistrarTest.java
@@ -7,13 +7,13 @@ package com.magento.idea.magento2plugin.completion.xml;
 
 import com.magento.idea.magento2plugin.magento.files.LayoutXml;
 
-public class LayoutContainerCompletionRegistrarTest extends CompletionXmlFixtureTestCase {
+public class XLayoutContainerCompletionRegistrarTest extends CompletionXmlFixtureTestCase {
 
     /**
      * The `name` attribute of the `referenceContainer` tag in layout XML must
      * have completion based on `name` attribute of `container` tags.
      */
-    public void testReferenceContainerMustHaveCompletion() {
+    public void testXReferenceContainerMustHaveCompletion() {
         final String filePath = this.getFixturePath(LayoutXml.DEFAULT_FILENAME);
         myFixture.configureByFile(filePath);
 
