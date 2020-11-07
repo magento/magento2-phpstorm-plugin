92939e8eaa3105093c2a12b22bc8804fd26b59f6
Adarsh Manickam <adarsh.apple@icloud.com>
2020-11-08 01:06:16 +0530

    Fixed static test failures

--- tests/com/magento/idea/magento2plugin/reference/php/DataFixtureReferenceRegistrarTest.java
+++ tests/com/magento/idea/magento2plugin/reference/php/DataFixtureReferenceRegistrarTest.java
@@ -8,11 +8,13 @@ package com.magento.idea.magento2plugin.reference.php;
 public class DataFixtureReferenceRegistrarTest extends ReferencePhpFixtureTestCase {
 
     /**
-     * Tests that file path of @magentoApiDataFixture tag has a reference
+     * Tests that file path of @magentoApiDataFixture tag has a reference.
      */
     public void testDataFixtureMustHaveReference() {
         myFixture.configureByFile(this.getFixturePath("ProductRepositoryInterfaceTest.php"));
 
-        assertHasReferenceToFile("dev/tests/integration/testsuite/Magento/Catalog/_files/product.php");
+        assertHasReferenceToFile(
+                "dev/tests/integration/testsuite/Magento/Catalog/_files/product.php"
+        );
     }
 }
