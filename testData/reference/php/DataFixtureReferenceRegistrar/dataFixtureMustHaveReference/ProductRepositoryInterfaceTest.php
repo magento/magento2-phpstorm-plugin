7e6e8606c8e535246a8778fdc4200830d334b694
Adarsh Manickam <adarsh.apple@icloud.com>
2020-11-08 00:47:16 +0530

    Added test for DataFixtureReferenceProvider

---
+++ testData/reference/php/DataFixtureReferenceRegistrar/dataFixtureMustHaveReference/ProductRepositoryInterfaceTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Catalog\Api;
+
+class ProductRepositoryInterfaceTest
+{
+    /**
+     * @magentoApiDataFixture Magento<caret>/Catalog/_files/product.php
+     */
+    public function testGet()
+    {}
+}
---
+++ tests/com/magento/idea/magento2plugin/reference/php/DataFixtureReferenceRegistrarTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.reference.php;
+
+public class DataFixtureReferenceRegistrarTest extends ReferencePhpFixtureTestCase {
+
+    /**
+     * Tests that file path of @magentoApiDataFixture tag has a reference
+     */
+    public void testDataFixtureMustHaveReference() {
+        myFixture.configureByFile(this.getFixturePath("ProductRepositoryInterfaceTest.php"));
+
+        assertHasReferenceToFile("dev/tests/integration/testsuite/Magento/Catalog/_files/product.php");
+    }
+}
