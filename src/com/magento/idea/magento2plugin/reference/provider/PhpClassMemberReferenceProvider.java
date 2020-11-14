becfcfd7a776f22c54cef53326d91448c8b143f7
Adarsh Manickam <adarsh.apple@icloud.com>
2020-11-14 21:05:58 +0530

    Improved tests logic

--- src/com/magento/idea/magento2plugin/reference/provider/PhpClassMemberReferenceProvider.java
+++ src/com/magento/idea/magento2plugin/reference/provider/PhpClassMemberReferenceProvider.java
@@ -2,6 +2,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.reference.provider;
 
 import com.intellij.openapi.util.TextRange;
@@ -16,46 +17,50 @@ import com.jetbrains.php.lang.psi.elements.PhpClassMember;
 import com.magento.idea.magento2plugin.reference.xml.PolyVariantReferenceBase;
 import com.magento.idea.magento2plugin.util.RegExUtil;
 import gnu.trove.THashSet;
-import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jetbrains.annotations.NotNull;
 
 public class PhpClassMemberReferenceProvider extends PsiReferenceProvider {
 
     @NotNull
     @Override
-    public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
-        List<PsiReference> psiReferences = new ArrayList<>();
-        final Collection<PhpClassMember> members = new THashSet<>();
+    public PsiReference[] getReferencesByElement(
+            @NotNull final PsiElement element,
+            @NotNull final ProcessingContext context
+    ) {
+        final String value = StringUtil.unquoteString(element.getText());
+        final Matcher matcher
+                = Pattern.compile(RegExUtil.XmlRegex.CLASS_MEMBER_NAME).matcher(value);
 
-        String origValue = element.getText();
-        String value = StringUtil.unquoteString(element.getText());
-
-        Matcher matcher = Pattern.compile(RegExUtil.XmlRegex.CLASS_MEMBER_NAME).matcher(value);
         if (!matcher.find()) {
             return PsiReference.EMPTY_ARRAY;
         }
 
-        String elementName = matcher.group(1);
-        String classFQN = value.substring(0, value.lastIndexOf("::"));
+        final List<PsiReference> psiReferences = new ArrayList<>();
+        final Collection<PhpClassMember> members = new THashSet<>();
+        final String elementName = matcher.group(1);
+        final String classFQN = value.substring(0, value.lastIndexOf("::"));
+        final PhpIndex phpIndex = PhpIndex.getInstance(element.getProject());
 
-        PhpIndex phpIndex = PhpIndex.getInstance(element.getProject());
         for (final PhpClass phpClass : phpIndex.getAnyByFQN(classFQN)) {
             members.addAll(phpClass.getFields());
             members.addAll(phpClass.getMethods());
             members.removeIf(c -> !c.getName().equals(elementName));
         }
 
-        if (members.size() > 0) {
-            TextRange range = new TextRange(
+        if (!members.isEmpty()) {
+            final String origValue = element.getText();
+            final TextRange range = new TextRange(
                     origValue.indexOf(elementName),
                     origValue.indexOf(elementName) + elementName.length()
             );
             psiReferences.add(new PolyVariantReferenceBase(element, range, members));
         }
-        return psiReferences.toArray(new PsiReference[psiReferences.size()]);
+
+        return psiReferences.toArray(new PsiReference[0]);
     }
 }
