00cf658ac046703742b39de6742515c8bbc6cdc0
Adarsh Manickam <adarsh.apple@icloud.com>
2020-12-01 18:44:41 +0530

    Covered ObserverDeclarationInspection with tests

---
+++ tests/com/magento/idea/magento2plugin/inspections/xml/ObserverDeclarationInspectionTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.inspections.xml;
+
+import com.magento.idea.magento2plugin.magento.files.ModuleEventsXml;
+
+public class ObserverDeclarationInspectionTest extends InspectionXmlFixtureTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        myFixture.enableInspections(ObserverDeclarationInspection.class);
+    }
+
+    /**
+     * Tests for duplicate observer name inspection warning in the same events.xml
+     */
+    public void testObserverNameUsedInSameFile() {
+        myFixture.configureByFile(getFixturePath(ModuleEventsXml.FILE_NAME));
+        myFixture.testHighlighting(true, false, false);
+    }
+
+    /**
+     * Tests for duplicate observer name inspection warning in a different events.xml
+     */
+    public void testObserverNameUsedInDifferentFile() {
+        myFixture.configureByFile(getFixturePath(ModuleEventsXml.FILE_NAME));
+        myFixture.testHighlighting(true, false, false);
+    }
+}
