8496f5201772912f879b5986fb51886a0ef92719
Vitaliy Boyko <v.boyko@atwix.com>
2020-12-02 10:44:55 +0200

    3.0.0 innitial

--- src/com/magento/idea/magento2plugin/linemarker/php/ClassConfigurationLineMarkerProvider.java
+++ src/com/magento/idea/magento2plugin/linemarker/php/ClassConfigurationLineMarkerProvider.java
@@ -1,7 +1,8 @@
-/**
+/*
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.linemarker.php;
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
@@ -13,18 +14,18 @@ import com.intellij.psi.xml.XmlTag;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.magento.idea.magento2plugin.indexes.XmlIndex;
 import com.magento.idea.magento2plugin.project.Settings;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by dkvashnin on 11/15/15.
  */
 public class ClassConfigurationLineMarkerProvider implements LineMarkerProvider {
+
     @Nullable
     @Override
     public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement psiElement) {
@@ -32,13 +33,13 @@ public class ClassConfigurationLineMarkerProvider implements LineMarkerProvider 
     }
 
     @Override
-    public void collectSlowLineMarkers(@NotNull List<PsiElement> list, @NotNull Collection<LineMarkerInfo> collection) {
-        if (list.size() > 0) {
-            if (!Settings.isEnabled(list.get(0).getProject())) {
+    public void collectSlowLineMarkers(@NotNull List<? extends PsiElement> elements, @NotNull Collection<? super LineMarkerInfo<?>> result) {
+        if (elements.size() > 0) {
+            if (!Settings.isEnabled(elements.get(0).getProject())) {
                 return;
             }
         }
-        for (PsiElement psiElement: list) {
+        for (PsiElement psiElement: elements) {
             if (psiElement instanceof PhpClass) {
                 List<XmlTag> results = new ArrayList<XmlTag>();
 
@@ -57,7 +58,7 @@ public class ClassConfigurationLineMarkerProvider implements LineMarkerProvider 
 
                 PsiElement className = ((PhpClass) psiElement).getNameIdentifier();
                 if (className != null) {
-                    collection.add(builder.createLineMarkerInfo(className));
+                    result.add(builder.createLineMarkerInfo(className));
                 }
             }
         }
