312ba7dd5ba088d8fc0d440b06d2314276488347
Adarsh Manickam <adarsh.apple@icloud.com>
2020-12-05 13:26:11 +0530

    Added Magento copyrights

--- src/com/magento/idea/magento2plugin/actions/generation/NewMessageQueueAction.java
+++ src/com/magento/idea/magento2plugin/actions/generation/NewMessageQueueAction.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 package com.magento.idea.magento2plugin.actions.generation;
 
 import com.intellij.ide.IdeView;
