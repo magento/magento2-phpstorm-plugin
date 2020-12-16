d957ffebd0f9419ff821d2a47568983b079e8709
Vitaliy Boyko <v.boyko@atwix.com>
2020-12-16 22:10:04 +0200

    Updated test framework to Junit 4 and fixed ignored tests

--- tests/com/magento/idea/magento2plugin/actions/generation/generator/BaseGeneratorTestCase.java
+++ tests/com/magento/idea/magento2plugin/actions/generation/generator/BaseGeneratorTestCase.java
@@ -6,18 +6,27 @@ package com.magento.idea.magento2plugin.actions.generation.generator;
 
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.LightPlatformTestCase;
 import com.magento.idea.magento2plugin.BaseProjectTestCase;
 import com.magento.idea.magento2plugin.magento.packages.File;
 import com.magento.idea.magento2plugin.project.util.GetProjectBasePath;
+import org.junit.Before;
 
 abstract public class BaseGeneratorTestCase extends BaseProjectTestCase {
     private static final String testDataFolderPath = "testData" + File.separator + "actions" + File.separator;
     private static final String fixturesFolderPath = "generation" + File.separator + "generator" + File.separator;
 
     @Override
+    @Before
     protected void setUp() throws Exception {
         super.setUp();
         myFixture.setTestDataPath(testDataFolderPath);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        LightPlatformTestCase.closeAndDeleteProject();
     }
 
     protected String getFixturePath(String fileName) {
