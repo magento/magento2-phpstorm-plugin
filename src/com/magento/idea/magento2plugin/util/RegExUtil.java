fd99c1d8df34cd7bcc727f4334352789111631ec
Adarsh Manickam <adarsh.manickam.muruganandam@gds.ey.com>
2020-12-17 19:17:03 +0530

    Fixed observer name validator

--- src/com/magento/idea/magento2plugin/util/RegExUtil.java
+++ src/com/magento/idea/magento2plugin/util/RegExUtil.java
@@ -28,6 +28,9 @@ public class RegExUtil {
     public static final String IDENTIFIER
             = "[a-zA-Z0-9_\\-]*";
 
+    public static final String IDENTIFIER_WITH_COLON
+            = "[a-zA-Z0-9:_\\-]*";
+
     public static final String LOWER_SNAKE_CASE
             = "[a-z][a-z0-9_]*";
 
