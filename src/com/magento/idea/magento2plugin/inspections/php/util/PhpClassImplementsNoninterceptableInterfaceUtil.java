6e3f896480f2edf6a01c0e932ec3a19aa30fb8b3
Vitaliy Boyko <v.boyko@atwix.com>
2021-01-10 19:34:25 +0200

    445: Static fixes

--- src/com/magento/idea/magento2plugin/inspections/php/util/PhpClassImplementsNoninterceptableInterfaceUtil.java
+++ src/com/magento/idea/magento2plugin/inspections/php/util/PhpClassImplementsNoninterceptableInterfaceUtil.java
@@ -10,22 +10,24 @@ import com.magento.idea.magento2plugin.magento.files.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class PhpClassImplementsNoninterceptableInterfaceUtil {
+
+    private PhpClassImplementsNoninterceptableInterfaceUtil() {}
+
     /**
      * Check whether class implements NoninterceptableInterface.
      *
      * @param phpClass PhpClass
      * @return bool
      */
-    public static boolean execute(@NotNull PhpClass phpClass) {
-        PhpClass[] interfaces = phpClass.getImplementedInterfaces();
+    public static boolean execute(final @NotNull PhpClass phpClass) {
+        final PhpClass[] interfaces = phpClass.getImplementedInterfaces();
         if (interfaces.length == 0) {
             return false;
         }
-        for (PhpClass targetInterfaceClass: interfaces) {
-            if (!targetInterfaceClass.getFQN().equals(Plugin.NON_INTERCEPTABLE_FQN)) {
-                continue;
+        for (final PhpClass targetInterfaceClass: interfaces) {
+            if (targetInterfaceClass.getFQN().equals(Plugin.NON_INTERCEPTABLE_FQN)) {
+                return true;
             }
-            return true;
         }
 
         return false;
