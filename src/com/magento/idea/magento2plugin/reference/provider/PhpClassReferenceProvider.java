e08518b3a1b24f85d740e35063ac983373cc6ea0
Vitaliy Boyko <v.boyko@atwix.com>
2021-02-17 13:42:52 +0200

    Static fixes

--- src/com/magento/idea/magento2plugin/reference/provider/PhpClassReferenceProvider.java
+++ src/com/magento/idea/magento2plugin/reference/provider/PhpClassReferenceProvider.java
@@ -1,7 +1,8 @@
-/**
+/*
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.reference.provider;
 
 import com.intellij.openapi.util.TextRange;
@@ -14,60 +15,69 @@ import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.PhpNamespace;
 import com.magento.idea.magento2plugin.reference.xml.PolyVariantReferenceBase;
 import com.magento.idea.magento2plugin.util.RegExUtil;
-import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings({"PMD.AvoidInstantiatingObjectsInLoops"})
 public class PhpClassReferenceProvider extends PsiReferenceProvider {
 
     @NotNull
     @Override
-    public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
-        List<PsiReference> psiReferences = new ArrayList<>();
+    public PsiReference[] getReferencesByElement(
+            final @NotNull PsiElement element,
+            final @NotNull ProcessingContext context
+    ) {
 
-        String origValue = element.getText();
+        final String origValue = element.getText();
 
-        Pattern pattern = Pattern.compile(RegExUtil.PhpRegex.FQN);
-        Matcher matcher = pattern.matcher(origValue);
+        final Pattern pattern = Pattern.compile(RegExUtil.PhpRegex.FQN);
+        final Matcher matcher = pattern.matcher(origValue);
         if (!matcher.find()) {
             return PsiReference.EMPTY_ARRAY;
         }
 
-        String classFQN = origValue.replaceAll("^\"|\"$", "");;
-        String[] fqnParts = classFQN.split("\\\\");
+        final String classFQN = origValue.replaceAll("^\"|\"$", "");
+        final String[] fqnParts = classFQN.split("\\\\");
 
-        PhpIndex phpIndex = PhpIndex.getInstance(element.getProject());
+        final PhpIndex phpIndex = PhpIndex.getInstance(element.getProject());
 
-        StringBuilder namespace = new StringBuilder();
+        final StringBuilder namespace = new StringBuilder();
         String namespacePart;
+        final List<PsiReference> psiReferences = new ArrayList<>();
         for (int i = 0; i < fqnParts.length - 1; i++) {
             namespacePart = fqnParts[i];
 
             namespace.append("\\");
             namespace.append(namespacePart);
-            Collection<PhpNamespace> references = phpIndex.getNamespacesByName(namespace.toString().toLowerCase());
-            if (references.size() > 0) {
-                TextRange range = new TextRange(
+            final Collection<PhpNamespace> references =
+                    phpIndex.getNamespacesByName(namespace.toString().toLowerCase(
+                            new Locale("en","EN"))
+                    );
+            if (!references.isEmpty()) {
+                final TextRange range = new TextRange(
                         origValue.indexOf(classFQN) + namespace.toString().lastIndexOf(92),
-                        origValue.indexOf(classFQN) + namespace.toString().lastIndexOf(92) + namespacePart.length()
+                        origValue.indexOf(classFQN) + namespace.toString().lastIndexOf(92)
+                                + namespacePart.length()
                 );
                 psiReferences.add(new PolyVariantReferenceBase(element, range, references));
             }
         }
 
-        String className = classFQN.substring(classFQN.lastIndexOf(92) + 1);
-        Collection<PhpClass> classes = phpIndex.getAnyByFQN(classFQN);
-        if (classes.size() > 0) {
-            TextRange range = new TextRange(
+        final String className = classFQN.substring(classFQN.lastIndexOf(92) + 1);
+        final Collection<PhpClass> classes = phpIndex.getAnyByFQN(classFQN);
+        if (!classes.isEmpty()) {
+            final TextRange range = new TextRange(
                     origValue.lastIndexOf(92) + 1,
                     origValue.lastIndexOf(92) + 1 + className.length()
             );
             psiReferences.add(new PolyVariantReferenceBase(element, range, classes));
         }
 
-        return psiReferences.toArray(new PsiReference[psiReferences.size()]);
+        return psiReferences.toArray(new PsiReference[0]);
     }
 }
