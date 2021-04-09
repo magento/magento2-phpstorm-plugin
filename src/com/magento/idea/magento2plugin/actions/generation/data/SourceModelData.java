f59095f61c8c7ea66b822c54ceccbcf048d54219
Vitaliy Prokopoov <v.prokopov@atwix.com>
2021-04-09 16:22:53 +0300

    Added copyright

--- src/com/magento/idea/magento2plugin/actions/generation/data/SourceModelData.java
+++ src/com/magento/idea/magento2plugin/actions/generation/data/SourceModelData.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 package com.magento.idea.magento2plugin.actions.generation.data;
 
 import com.magento.idea.magento2plugin.magento.files.SourceModelPhp;
--- src/com/magento/idea/magento2plugin/magento/packages/uicomponent/AvailableSourcesByInput.java
+++ src/com/magento/idea/magento2plugin/magento/packages/uicomponent/AvailableSourcesByInput.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 package com.magento.idea.magento2plugin.magento.packages.uicomponent;
 
 import com.magento.idea.magento2plugin.actions.generation.data.ui.ComboBoxItemData;
