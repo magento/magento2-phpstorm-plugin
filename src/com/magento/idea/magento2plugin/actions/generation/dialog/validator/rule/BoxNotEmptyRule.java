d1a97be200d7e187fa0d5d529a5a5f790027c272
Serhii Akulov <s.akulov@atwix.com>
2021-04-16 21:01:05 +0300

    Fixed copyrights, indentation and added @NotNull to methods

--- src/com/magento/idea/magento2plugin/actions/generation/dialog/validator/rule/BoxNotEmptyRule.java
+++ src/com/magento/idea/magento2plugin/actions/generation/dialog/validator/rule/BoxNotEmptyRule.java
@@ -5,12 +5,15 @@
 
 package com.magento.idea.magento2plugin.actions.generation.dialog.validator.rule;
 
+import org.jetbrains.annotations.NotNull;
+
 public class BoxNotEmptyRule implements ValidationRule {
+
     public static final String MESSAGE = "validator.box.notEmpty";
     private static final ValidationRule INSTANCE = new BoxNotEmptyRule();
 
     @Override
-    public boolean check(final String value) {
+    public boolean check(final @NotNull String value) {
         return !value.isEmpty();
     }
 
