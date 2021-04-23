80ecb5d90aeb52fd043f4efb76f612d7e7b449ed
Vitaliy Boyko <v.boyko@atwix.com>
2021-04-23 16:04:41 +0300

    Static fixes 4

--- src/com/magento/idea/magento2plugin/MagentoIcons.java
+++ src/com/magento/idea/magento2plugin/MagentoIcons.java
@@ -8,6 +8,7 @@ package com.magento.idea.magento2plugin;
 import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
+@SuppressWarnings({"PMD.ClassNamingConventions"})
 public class MagentoIcons {
     public static final Icon WEB_API = IconLoader.getIcon("/icons/webapi.png", MagentoIcons.class);
     public static final Icon MODULE = IconLoader.getIcon("/icons/module.png", MagentoIcons.class);
