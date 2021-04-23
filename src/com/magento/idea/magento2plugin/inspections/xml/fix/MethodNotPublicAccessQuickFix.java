1577bd2587f9696c1034f641eec30094be485fb5
Vitaliy Boyko <v.boyko@atwix.com>
2021-04-23 15:58:31 +0300

    Static fixes 3

--- src/com/magento/idea/magento2plugin/inspections/xml/fix/MethodNotPublicAccessQuickFix.java
+++ src/com/magento/idea/magento2plugin/inspections/xml/fix/MethodNotPublicAccessQuickFix.java
@@ -7,7 +7,6 @@ package com.magento.idea.magento2plugin.inspections.xml.fix;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
-import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.lang.inspections.quickfix.PhpChangeMethodModifiersQuickFix;
 import com.jetbrains.php.lang.psi.elements.Method;
@@ -16,10 +15,10 @@ import com.magento.idea.magento2plugin.bundles.InspectionBundle;
 import org.jetbrains.annotations.NotNull;
 
 public class MethodNotPublicAccessQuickFix implements LocalQuickFix {
-    private InspectionBundle inspectionBundle = new InspectionBundle();
-    private Method method;
+    private final InspectionBundle inspectionBundle = new InspectionBundle();
+    private final Method method;
 
-    public MethodNotPublicAccessQuickFix(Method method) {
+    public MethodNotPublicAccessQuickFix(final Method method) {
         this.method = method;
     }
 
@@ -30,7 +29,10 @@ public class MethodNotPublicAccessQuickFix implements LocalQuickFix {
     }
 
     @Override
-    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    public void applyFix(
+            final @NotNull Project project,
+            final @NotNull ProblemDescriptor descriptor
+    ) {
         PhpChangeMethodModifiersQuickFix.changeMethodModifier(
                 this.method,
                 PhpModifier.PUBLIC_IMPLEMENTED_DYNAMIC
