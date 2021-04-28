e03abae3d7f89a93ae3bc1e179c28b7ff6b45580
Serhii Akulov <s.akulov@atwix.com>
2021-04-28 22:26:59 +0300

    refactoring code and adding tests

--- src/com/magento/idea/magento2plugin/inspections/xml/PreferenceDeclarationInspection.java
+++ src/com/magento/idea/magento2plugin/inspections/xml/PreferenceDeclarationInspection.java
@@ -7,23 +7,21 @@ package com.magento.idea.magento2plugin.inspections.xml;
 
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.codeInspection.XmlSuppressableInspectionTool;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.XmlElementVisitor;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlAttribute;
-import com.intellij.psi.xml.XmlDocument;
 import com.intellij.psi.xml.XmlTag;
-import com.jetbrains.php.lang.inspections.PhpInspection;
-import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.magento.idea.magento2plugin.bundles.InspectionBundle;
+import com.magento.idea.magento2plugin.inspections.validator.InspectionValidator;
+import com.magento.idea.magento2plugin.inspections.validator.NotEmptyValidator;
+import com.magento.idea.magento2plugin.inspections.validator.PhpClassExistenceValidator;
 import com.magento.idea.magento2plugin.magento.files.ModuleDiXml;
-import com.magento.idea.magento2plugin.util.GetPhpClassByFQN;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-@SuppressWarnings({"PMD.ExcessiveMethodLength", "PMD.NPathComplexity"})
-public class PreferenceDeclarationInspection extends PhpInspection {
+public class PreferenceDeclarationInspection extends XmlSuppressableInspectionTool {
 
     @Override
     public @NotNull PsiElementVisitor buildVisitor(
@@ -33,99 +31,103 @@ public class PreferenceDeclarationInspection extends PhpInspection {
         return new XmlElementVisitor() {
             private final InspectionBundle inspectionBundle = new InspectionBundle();
 
+            private final InspectionValidator phpClassExistenceValidator =
+                    new PhpClassExistenceValidator(problemsHolder.getProject());
+            private final InspectionValidator notEmptyValidator = new NotEmptyValidator();
+
             @Override
-            public void visitFile(final @NotNull PsiFile file) {
-                if (!file.getName().equals(ModuleDiXml.FILE_NAME)) {
+            public void visitXmlTag(final XmlTag xmlTag) {
+                final PsiFile file = xmlTag.getContainingFile();
+
+                if (!file.getName().equals(ModuleDiXml.FILE_NAME)
+                        || !xmlTag.getName().equals(ModuleDiXml.PREFERENCE_TAG_NAME)) {
                     return;
                 }
 
-                final XmlTag[] xmlTags = getFileXmlTags(file);
+                final XmlAttribute preferenceForAttribute =
+                        xmlTag.getAttribute(ModuleDiXml.PREFERENCE_ATTR_FOR);
+                final XmlAttribute preferenceTypeAttribute =
+                        xmlTag.getAttribute(ModuleDiXml.TYPE_ATTR);
 
-                if (xmlTags == null) {
+                if (preferenceForAttribute == null
+                        || preferenceForAttribute.getValue() == null
+                        || preferenceForAttribute.getValueElement() == null) {
                     return;
                 }
 
-                for (final XmlTag preferenceXmlTag : xmlTags) {
-                    if (!preferenceXmlTag.getName().equals(ModuleDiXml.PREFERENCE_TAG_NAME)) {
-                        continue;
-                    }
+                if (!notEmptyValidator.validate(preferenceForAttribute.getValue())) {
+                    reportCouldNotBeEmpty(
+                            preferenceForAttribute.getValueElement(),
+                            preferenceForAttribute.getName()
+                    );
+                }
 
-                    final XmlAttribute preferenceForAttribute =
-                            preferenceXmlTag.getAttribute(ModuleDiXml.PREFERENCE_ATTR_FOR);
-                    final XmlAttribute preferenceTypeAttribute =
-                            preferenceXmlTag.getAttribute(ModuleDiXml.TYPE_ATTR);
+                if (preferenceTypeAttribute == null
+                        || preferenceTypeAttribute.getValue() == null
+                        || preferenceTypeAttribute.getValueElement() == null) {
+                    return;
+                }
 
-                    if (preferenceForAttribute == null || preferenceTypeAttribute == null) {
-                        continue;
-                    }
+                if (!notEmptyValidator.validate(preferenceTypeAttribute.getValue())) {
+                    reportCouldNotBeEmpty(
+                            preferenceTypeAttribute.getValueElement(),
+                            preferenceTypeAttribute.getName()
+                    );
+                }
 
-                    final Boolean isPreferenceForClassExists =
-                            isXmlAttributeValueClassExists(preferenceForAttribute);
+                if (!phpClassExistenceValidator.validate(preferenceForAttribute.getValue())) {
+                    reportClassDoesNotExists(
+                            preferenceForAttribute.getValueElement(),
+                            preferenceForAttribute.getValue()
+                    );
+                }
 
-                    if (isPreferenceForClassExists != null && !isPreferenceForClassExists) {
-                        reportClassDoesntExistsProblem(preferenceForAttribute);
-                    }
-
-                    final Boolean isPreferenceTypeClassExists =
-                            isXmlAttributeValueClassExists(preferenceTypeAttribute);
-
-                    if (isPreferenceTypeClassExists != null && !isPreferenceTypeClassExists) {
-                        reportClassDoesntExistsProblem(preferenceTypeAttribute);
-                    }
+                if (!phpClassExistenceValidator.validate(preferenceTypeAttribute.getValue())) {
+                    reportClassDoesNotExists(
+                            preferenceTypeAttribute.getValueElement(),
+                            preferenceTypeAttribute.getValue()
+                    );
                 }
             }
 
             /**
-             * Output a warning in the xml class.
+             * Report Attribute Value could not be empty.
              *
-             * @param xmlAttribute XmlAttribute
+             * @param psiElement PsiElement
+             * @param messageParams Object...
              */
-            private void reportClassDoesntExistsProblem(final @NotNull XmlAttribute xmlAttribute) {
-                if (xmlAttribute.getValueElement() == null) {
-                    return;
-                }
+            private void reportCouldNotBeEmpty(
+                    final @NotNull PsiElement psiElement,
+                    final Object... messageParams
+            ) {
                 problemsHolder.registerProblem(
-                        xmlAttribute.getValueElement(),
-                        inspectionBundle.message("inspection.preference.error.notExist"),
-                        ProblemHighlightType.WARNING
+                        psiElement,
+                        inspectionBundle.message(
+                                "inspection.error.idAttributeCanNotBeEmpty",
+                                messageParams
+                        ),
+                        ProblemHighlightType.ERROR
                 );
             }
 
             /**
-             * Checks the existence of the php class in the specified directory.
+             * Report class does not exists.
              *
-             * @param xmlAttribute XmlAttribute
-             *
-             * @return attributeValueClass
+             * @param psiElement PsiElement
+             * @param messageParams Object...
              */
-            private @Nullable Boolean isXmlAttributeValueClassExists(
-                    final @NotNull XmlAttribute xmlAttribute
+            private void reportClassDoesNotExists(
+                    final @NotNull PsiElement psiElement,
+                    final Object... messageParams
             ) {
-                final String attributeValue = xmlAttribute.getValue();
-
-                if (attributeValue == null) {
-                    return null;
-                }
-
-                final PhpClass attributeValueClass =
-                        GetPhpClassByFQN.getInstance(xmlAttribute.getProject())
-                                .execute(attributeValue);
-
-                return attributeValueClass != null;
-            }
-
-            /**
-             * Get all children xml tags for root xml tag of file.
-             *
-             * @param file PsiFile
-             *
-             * @return XmlTag[]
-             */
-            private @Nullable XmlTag[] getFileXmlTags(final @NotNull PsiFile file) {
-                final XmlDocument xmlDocument = PsiTreeUtil.getChildOfType(file, XmlDocument.class);
-                final XmlTag xmlRootTag = PsiTreeUtil.getChildOfType(xmlDocument, XmlTag.class);
-
-                return PsiTreeUtil.getChildrenOfType(xmlRootTag, XmlTag.class);
+                problemsHolder.registerProblem(
+                        psiElement,
+                        inspectionBundle.message(
+                                "inspection.warning.class.does.not.exist",
+                                messageParams
+                        ),
+                        ProblemHighlightType.WARNING
+                );
             }
         };
     }
---
+++ tests/com/magento/idea/magento2plugin/inspections/xml/PreferenceDeclarationInspectionTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.inspections.xml;
+
+import com.magento.idea.magento2plugin.magento.files.ModuleDiXml;
+
+public class PreferenceDeclarationInspectionTest extends InspectionXmlFixtureTestCase {
+
+    private static final String ARGUMENT_VALUE_IS_EMPTY =
+            "inspection.error.idAttributeCanNotBeEmpty";
+
+    private static final String CLASS_DOES_NOT_EXIST =
+            "inspection.warning.class.does.not.exist";
+
+    private static final String EXISTENT_CLASS_ONE =
+            "Magento\\Catalog\\Api\\ProductRepositoryInterface";
+
+    private static final String EXISTENT_CLASS_TWO =
+            "Foo\\Bar\\Model\\Logger";
+
+    private static final String NOT_EXISTENT_CLASS =
+            "Not\\Existent\\Class";
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        myFixture.enableInspections(PreferenceDeclarationInspection.class);
+    }
+
+    /**
+     * Test for the absence of an error in the presence of
+     * classes or interfaces specified for preferences
+     * <preference for="Magento\Catalog\Api\ProductRepositoryInterface" type="Foo\Bar\Model\Logger"/>
+     */
+    public void testClassAttrForAndTypeAreExist() {
+        configureFixture();
+
+        final String classOneExists =  inspectionBundle.message(
+                CLASS_DOES_NOT_EXIST,
+                EXISTENT_CLASS_ONE
+        );
+
+        assertHasNoHighlighting(classOneExists);
+
+        final String classTwoExists =  inspectionBundle.message(
+                CLASS_DOES_NOT_EXIST,
+                EXISTENT_CLASS_TWO
+        );
+
+        assertHasNoHighlighting(classTwoExists);
+    }
+
+    /**
+     * Test for throwing an error for a class that does not exist for the "for" attribute
+     */
+    public void testClassAttrForDoesNotExists() {
+        configureFixture();
+
+        final String forClassDoesNotExists =  inspectionBundle.message(
+                CLASS_DOES_NOT_EXIST,
+                NOT_EXISTENT_CLASS
+        );
+
+        assertHasHighlighting(forClassDoesNotExists);
+    }
+
+    /**
+     * Test for throwing an error for a class that does not exist for the "type" attribute
+     */
+    public void testClassAttrTypeDoesNotExists() {
+        configureFixture();
+
+        final String forClassDoesNotExists =  inspectionBundle.message(
+                CLASS_DOES_NOT_EXIST,
+                NOT_EXISTENT_CLASS
+        );
+
+        assertHasHighlighting(forClassDoesNotExists);
+    }
+
+    /**
+     * Test for an error for the "type" attribute because it is empty and
+     * there is no error "for" the for attribute because this class exists
+     * <preference for="Foo\Bar\Model\Logger" type=""/>
+     */
+    public void testForAttrArgsValuesIsEmpty() {
+        configureFixture();
+
+        final String forAttrIsEmptyMessage =  inspectionBundle.message(
+                ARGUMENT_VALUE_IS_EMPTY,
+                ModuleDiXml.PREFERENCE_ATTR_FOR
+        );
+
+        assertHasHighlighting(forAttrIsEmptyMessage);
+
+        final String typeAttrIsEmptyMessage =  inspectionBundle.message(
+                ARGUMENT_VALUE_IS_EMPTY,
+                ModuleDiXml.TYPE_ATTR
+        );
+
+        assertHasNoHighlighting(typeAttrIsEmptyMessage);
+    }
+
+    /**
+     * Test for an error for the "for" attribute because it is empty and
+     * there is no error "type" the for attribute because this class exists
+     * <preference for="" type="Foo\Bar\Model\Logger"/>
+     */
+    public void testTypeAttrArgsValuesIsEmpty() {
+        configureFixture();
+
+        final String forAttrIsEmptyMessage =  inspectionBundle.message(
+                ARGUMENT_VALUE_IS_EMPTY,
+                ModuleDiXml.TYPE_ATTR
+        );
+
+        assertHasHighlighting(forAttrIsEmptyMessage);
+
+        final String typeAttrIsEmptyMessage =  inspectionBundle.message(
+                ARGUMENT_VALUE_IS_EMPTY,
+                ModuleDiXml.PREFERENCE_ATTR_FOR
+        );
+
+        assertHasNoHighlighting(typeAttrIsEmptyMessage);
+    }
+
+    private void configureFixture() {
+        myFixture.configureByFile(getFixturePath(ModuleDiXml.FILE_NAME));
+    }
+}
