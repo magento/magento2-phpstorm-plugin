173cdc52eca33856ddf24d1f585e23733874c1ec
Serhii Akulov <s.akulov@atwix.com>
2021-07-29 14:52:20 +0300

    Fixed tests

--- src/com/magento/idea/magento2plugin/actions/generation/dialog/OverrideInThemeDialog.java
+++ src/com/magento/idea/magento2plugin/actions/generation/dialog/OverrideInThemeDialog.java
@@ -135,8 +135,9 @@ public class OverrideInThemeDialog extends AbstractDialog {
     private void fillThemeOptions() {
         final String area = psiFile.getVirtualFile().getPath().split("view/")[1].split("/")[0];
         final List<String> themeNames = new ModuleIndex(project).getEditableThemeNames();
+        final String baseArea = "base";
         for (final String themeName: themeNames) {
-            if (area.equals("base") || themeName.split("/")[0].equals(area)) {
+            if (baseArea.equals(area) || themeName.split("/")[0].equals(area)) {
                 theme.addItem(themeName);
             }
         }
