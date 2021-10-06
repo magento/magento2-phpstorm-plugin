111d80f8986d75c0789d8d2b77e10537fcda61ad
RadionBorysov <r.borysov@atwix.com>
2021-10-06 12:12:22 +0300

    Double quotes php-inspections

---
+++ src/com/magento/idea/magento2plugin/inspections/php/DoubleQuotesPhpInspection.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.inspections.php;
+
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.inspections.PhpInspection;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
+import com.magento.idea.magento2plugin.bundles.InspectionBundle;
+import org.jetbrains.annotations.NotNull;
+
+
+public class DoubleQuotesPhpInspection extends PhpInspection {
+
+    @NotNull
+    @Override
+    public PsiElementVisitor buildVisitor(
+            final @NotNull ProblemsHolder problemsHolder,
+            final boolean isOnTheFly
+    ) {
+        return new PhpElementVisitor() {
+
+            @Override
+            public void visitPhpStringLiteralExpression(final StringLiteralExpression expression) {
+                final String fileText = expression.getText();
+
+                if (fileText.charAt(1) != '"') {
+                    return;
+                }
+
+                if (fileText.contains("$")) {
+                    return;
+                }
+
+                final InspectionBundle inspectionBundle = new InspectionBundle();
+                problemsHolder.registerProblem(
+                        expression,
+                        inspectionBundle.message(
+                                "inspection.displayName.DoubleQuotesPhpInspection"
+                        ),
+                        ProblemHighlightType.WARNING
+                );
+            }
+        };
+    }
+}
