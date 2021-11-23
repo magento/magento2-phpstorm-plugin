964e88fddf909b0592528a44ae74fa8530101fb6
bohdan-harniuk <b.harniuk@atwix.com>
2021-11-23 13:45:43 +0100

    UCT-725: Developed used non api reference inspection, added factory and proxy types support

--- src/com/magento/idea/magento2uct/inspections/php/UsedTypeInspection.java
+++ src/com/magento/idea/magento2uct/inspections/php/UsedTypeInspection.java
@@ -11,12 +11,16 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.jetbrains.php.lang.inspections.PhpInspection;
 import com.jetbrains.php.lang.psi.elements.ClassReference;
+import com.jetbrains.php.lang.psi.elements.ExtendsList;
+import com.jetbrains.php.lang.psi.elements.ImplementsList;
 import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.PhpUse;
 import com.jetbrains.php.lang.psi.resolve.types.PhpTypeAnalyserVisitor;
 import com.magento.idea.magento2plugin.magento.packages.MagentoPhpClass;
 import com.magento.idea.magento2uct.packages.IssueSeverityLevel;
 import com.magento.idea.magento2uct.settings.UctSettingsService;
+import com.magento.idea.magento2uct.util.php.ReferenceResolverUtil;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class UsedTypeInspection extends PhpInspection {
@@ -30,6 +34,11 @@ public abstract class UsedTypeInspection extends PhpInspection {
 
             @Override
             public void visitPhpClassReference(final ClassReference reference) {
+                if ((reference.getContext() instanceof PhpUse)
+                        || (reference.getContext() instanceof ExtendsList)
+                        || (reference.getContext() instanceof ImplementsList)) {
+                    return;
+                }
                 final Project project = reference.getProject();
                 final UctSettingsService settings = UctSettingsService.getInstance(project);
 
@@ -37,7 +46,7 @@ public abstract class UsedTypeInspection extends PhpInspection {
                         || !settings.isIssueLevelSatisfiable(getSeverityLevel())) {
                     return;
                 }
-                PsiElement resolved = reference.resolve();
+                PsiElement resolved = ReferenceResolverUtil.resolve(reference);
 
                 if (resolved instanceof Method
                         && MagentoPhpClass.CONSTRUCT_METHOD_NAME
