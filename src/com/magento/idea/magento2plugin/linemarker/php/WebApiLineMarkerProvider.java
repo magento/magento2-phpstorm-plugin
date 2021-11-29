7293944f8815382c4d4e696ffba2784cb16acfba
bohdan-harniuk <b.harniuk@atwix.com>
2021-11-29 14:37:17 +0100

    Bugfix-765: Code refactoring

--- src/com/magento/idea/magento2plugin/linemarker/php/WebApiLineMarkerProvider.java
+++ src/com/magento/idea/magento2plugin/linemarker/php/WebApiLineMarkerProvider.java
@@ -1,7 +1,8 @@
-/**
+/*
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.linemarker.php;
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
@@ -15,10 +16,13 @@ import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.magento.idea.magento2plugin.MagentoIcons;
 import com.magento.idea.magento2plugin.project.Settings;
 import com.magento.idea.magento2plugin.stubs.indexes.WebApiTypeIndex;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.*;
 
 /**
  * Line marker for methods and classes which are exposed as web APIs.
@@ -28,34 +32,42 @@ import java.util.*;
  */
 public class WebApiLineMarkerProvider implements LineMarkerProvider {
 
-    @Nullable
     @Override
-    public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement psiElement) {
+    public @Nullable LineMarkerInfo<?> getLineMarkerInfo(final @NotNull PsiElement psiElement) {
         return null;
     }
 
     @Override
-    public void collectSlowLineMarkers(@NotNull List<? extends PsiElement> psiElements, @NotNull Collection<? super LineMarkerInfo<?>> collection) {
-        if (psiElements.size() > 0) {
-            if (!Settings.isEnabled(psiElements.get(0).getProject())) {
-                return;
-            }
+    public void collectSlowLineMarkers(
+            final @NotNull List<? extends PsiElement> psiElements,
+            final @NotNull Collection<? super LineMarkerInfo<?>> collection
+    ) {
+        if (psiElements.isEmpty()) {
+            return;
         }
-        for (PsiElement psiElement: psiElements) {
-            WebApiRoutesCollector collector = new WebApiRoutesCollector();
+
+        if (!Settings.isEnabled(psiElements.get(0).getProject())) {
+            return;
+        }
+
+        for (final PsiElement psiElement : psiElements) {
+            final WebApiRoutesCollector collector = new WebApiRoutesCollector();
             List<XmlTag> results = new ArrayList<>();
+
             if (psiElement instanceof Method) {
                 results = collector.getRoutes((Method) psiElement);
             } else if (psiElement instanceof PhpClass) {
                 results = collector.getRoutes((PhpClass) psiElement);
             }
 
-            if (!(results.size() > 0)) {
+            if (results.isEmpty()) {
                 continue;
             }
 
-            StringBuilder tooltipText = new StringBuilder("Navigate to Web API configuration:<pre>");
-            for (XmlTag routeTag : results) {
+            StringBuilder tooltipText = new StringBuilder(
+                    "Navigate to Web API configuration:<pre>"
+            );
+            for (final XmlTag routeTag : results) {
                 tooltipText.append(routeTag.getName()).append("\n");
             }
             tooltipText.append("</pre>");
@@ -72,24 +84,27 @@ public class WebApiLineMarkerProvider implements LineMarkerProvider {
      */
     private static class WebApiRoutesCollector {
 
-        private HashMap<String, List<XmlTag>> routesCache = new HashMap<>();
-
-        private static final Map<String, Integer> HTTP_METHODS_SORT_ORDER = new HashMap<String, Integer>() {{
-            put("GET", 1);
-            put("PUT", 2);
-            put("POS", 3);
-            put("DEL", 4);
-        }};
+        private final Map<String, List<XmlTag>> routesCache = new HashMap<>();
+        private static final Map<String, Integer> HTTP_METHODS_SORT_ORDER = new HashMap<>() {
+            {
+                put("GET", 1);
+                put("PUT", 2);
+                put("POS", 3);
+                put("DEL", 4);
+            }
+        };
 
         /**
          * Get sorted list of Web API routes related to the specified class.
          */
-        List<XmlTag> getRoutes(@NotNull PhpClass phpClass) {
-            List<XmlTag> routesForClass = new ArrayList<>();
-            for (Method method : phpClass.getMethods()) {
+        public List<XmlTag> getRoutes(final @NotNull PhpClass phpClass) {
+            final List<XmlTag> routesForClass = new ArrayList<>();
+
+            for (final Method method : phpClass.getMethods()) {
                 routesForClass.addAll(getRoutes(method));
             }
             sortRoutes(routesForClass);
+
             return routesForClass;
         }
 
@@ -98,13 +113,15 @@ public class WebApiLineMarkerProvider implements LineMarkerProvider {
          * <p/>
          * Results are cached.
          */
-        List<XmlTag> getRoutes(@NotNull Method method) {
-            String methodFqn = method.getFQN();
+        public List<XmlTag> getRoutes(final @NotNull Method method) {
+            final String methodFqn = method.getFQN();
+
             if (!routesCache.containsKey(methodFqn)) {
                 List<XmlTag> routesForMethod = extractRoutesForMethod(method);
                 sortRoutes(routesForMethod);
                 routesCache.put(methodFqn, routesForMethod);
             }
+
             return routesCache.get(methodFqn);
         }
 
@@ -114,37 +131,42 @@ public class WebApiLineMarkerProvider implements LineMarkerProvider {
          * Web API declarations for parent classes are taken into account.
          * Results are not cached.
          */
-        List<XmlTag> extractRoutesForMethod(@NotNull Method method) {
-            List<XmlTag> routesForMethod = WebApiTypeIndex.getWebApiRoutes(method);
-            PhpClass phpClass = method.getContainingClass();
+        public List<XmlTag> extractRoutesForMethod(final @NotNull Method method) {
+            final List<XmlTag> routesForMethod = WebApiTypeIndex.getWebApiRoutes(method);
+            final PhpClass phpClass = method.getContainingClass();
+
             if (phpClass == null) {
                 return routesForMethod;
             }
-            for (PhpClass parent : method.getContainingClass().getSupers()) {
+            for (final PhpClass parent : method.getContainingClass().getSupers()) {
                 for (Method parentMethod : parent.getMethods()) {
                     if (parentMethod.getName().equals(method.getName())) {
                         routesForMethod.addAll(extractRoutesForMethod(parentMethod));
                     }
                 }
             }
+
             return routesForMethod;
         }
 
         /**
          * Make sure that routes are sorted as follows: GET, PUT, POST, DELETE. Then by path.
          */
-        private void sortRoutes(List<XmlTag> routes) {
+        private void sortRoutes(final List<XmlTag> routes) {
             routes.sort(
-                (firstTag, secondTag) -> {
-                    String substring = firstTag.getName().substring(2, 5);
-                    Integer firstSortOrder = HTTP_METHODS_SORT_ORDER.get(substring);
-                    Integer secondSortOrder = HTTP_METHODS_SORT_ORDER.get(secondTag.getName().substring(2, 5));
-                    if (firstSortOrder.compareTo(secondSortOrder) == 0) {
-                        // Sort by route if HTTP methods are equal
-                        return firstTag.getName().compareTo(secondTag.getName());
+                    (firstTag, secondTag) -> {
+                        String substring = firstTag.getName().substring(2, 5);
+                        Integer firstSortOrder = HTTP_METHODS_SORT_ORDER.get(substring);
+                        Integer secondSortOrder = HTTP_METHODS_SORT_ORDER.get(
+                                secondTag.getName().substring(2, 5)
+                        );
+                        if (firstSortOrder.compareTo(secondSortOrder) == 0) {
+                            // Sort by route if HTTP methods are equal
+                            return firstTag.getName().compareTo(secondTag.getName());
+                        }
+
+                        return firstSortOrder.compareTo(secondSortOrder);
                     }
-                    return firstSortOrder.compareTo(secondSortOrder);
-                }
             );
         }
     }
