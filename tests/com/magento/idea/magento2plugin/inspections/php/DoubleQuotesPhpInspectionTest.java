2b98a5e136f18f7c02a997305c3b6a8cc73a62b0
Vitaliy Boyko <v.boyko@atwix.com>
2022-01-09 18:12:59 +0200

    Fixed static tests

--- tests/com/magento/idea/magento2plugin/inspections/php/DoubleQuotesPhpInspectionTest.java
+++ tests/com/magento/idea/magento2plugin/inspections/php/DoubleQuotesPhpInspectionTest.java
@@ -2,12 +2,13 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.inspections.php;
 
 public class DoubleQuotesPhpInspectionTest extends InspectionPhpFixtureTestCase {
 
     private final String errorMessage =  inspectionBundle.message(
-        "inspection.warning.double.quotes.misuse"
+            "inspection.warning.double.quotes.misuse"
     );
 
     @Override
@@ -21,18 +22,27 @@ public class DoubleQuotesPhpInspectionTest extends InspectionPhpFixtureTestCase 
         return false;
     }
 
+    /**
+     * Double quotes without variables should be highlighted.
+     */
     public void testDoubleQuotesWithoutVariables() throws Exception {
         myFixture.configureByFile(getFixturePath("ClassIndex.php"));
 
         assertHasHighlighting(errorMessage);
     }
 
+    /**
+     * Double quotes with variables should not be highlighted.
+     */
     public void testDoubleQuotesWithVariables() throws Exception {
         myFixture.configureByFile(getFixturePath("ClassIndex.php"));
 
         assertHasNoHighlighting(errorMessage);
     }
 
+    /**
+     * Single quotes should not be highlighted.
+     */
     public void testSingleQuotes() throws Exception {
         myFixture.configureByFile(getFixturePath("ClassIndex.php"));
 
