0a5cf4bc73a7d281bf7a3f6c3d4638251a223986
Mykola Donin <m.donin@atwix.com>
2022-02-11 16:47:36 +0200

    975: Added a new context dependent Action for the config.xml file generation

---
+++ src/com/magento/idea/magento2plugin/magento/files/ModuleConfigXmlFile.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.magento.files;
+
+import com.intellij.lang.Language;
+import com.intellij.lang.xml.XMLLanguage;
+
+public final class ModuleConfigXmlFile implements ModuleFileInterface {
+
+    public static final String FILE_NAME = "config.xml";
+    public static final String TEMPLATE = "Magento Config XML";
+
+    @Override
+    public String getFileName() {
+        return FILE_NAME;
+    }
+
+    @Override
+    public String getTemplate() {
+        return TEMPLATE;
+    }
+
+    @Override
+    public Language getLanguage() {
+        return XMLLanguage.INSTANCE;
+    }
+}
