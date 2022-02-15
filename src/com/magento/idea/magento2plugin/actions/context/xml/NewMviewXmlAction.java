a4038f6e0bfe19cf2aa5018bd153857bb0f8dfd5
Mykola Donin <m.donin@atwix.com>
2022-02-15 12:00:25 +0200

    985: Added mview.xml file in context generation.

---
+++ src/com/magento/idea/magento2plugin/actions/context/xml/NewMviewXmlAction.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.actions.context.xml;
+
+import com.intellij.ide.fileTemplates.actions.AttributesDefaults;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.magento.idea.magento2plugin.actions.context.AbstractContextAction;
+import com.magento.idea.magento2plugin.magento.files.ModuleMviewXmlFile;
+import com.magento.idea.magento2plugin.magento.packages.ComponentType;
+import com.magento.idea.magento2plugin.magento.packages.Package;
+import com.magento.idea.magento2plugin.util.magento.GetMagentoModuleUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class NewMviewXmlAction extends AbstractContextAction {
+
+    public static final String ACTION_NAME = "Magento 2 Mview File";
+    public static final String ACTION_DESCRIPTION = "Create a new Magento 2 mview.xml file";
+
+    /**
+     * New mview.xml file generation action constructor.
+     */
+    public NewMviewXmlAction() {
+        super(ACTION_NAME, ACTION_DESCRIPTION, new ModuleMviewXmlFile());
+    }
+
+    @Override
+    protected boolean isVisible(
+            final @NotNull GetMagentoModuleUtil.MagentoModuleData moduleData,
+            final @NotNull PsiDirectory targetDirectory,
+            final PsiFile targetFile
+    ) {
+        final PsiDirectory configDir = moduleData.getConfigDir();
+
+        if (configDir == null) {
+            return false;
+        }
+
+        return targetDirectory.getName().equals(Package.moduleBaseAreaDir)
+                && targetDirectory.equals(configDir)
+                && moduleData.getType().equals(ComponentType.module);
+    }
+
+
+    @Override
+    protected AttributesDefaults getProperties(
+            final @NotNull AttributesDefaults defaults,
+            final @NotNull GetMagentoModuleUtil.MagentoModuleData moduleData,
+            final PsiDirectory targetDirectory,
+            final PsiFile targetFile
+    ) {
+        return defaults;
+    }
+}
