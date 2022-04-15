499513d7bd236bfbdee49a1fd51887f5125d0651
bohdan-harniuk <b.harniuk@atwix.com>
2022-04-15 21:38:26 +0300

    600: Developed configurations autocompletion

--- src/com/magento/idea/magento2plugin/magento/files/ModuleSystemXmlFile.java
+++ src/com/magento/idea/magento2plugin/magento/files/ModuleSystemXmlFile.java
@@ -18,6 +18,12 @@ public final class ModuleSystemXmlFile implements ModuleFileInterface {
     public static final String XML_TAG_FRONTEND_MODEL = "frontend_model";
     public static final String XML_TAG_BACKEND_MODEL = "backend_model";
 
+    public static final String ROOT_TAG_NAME = "config";
+    public static final String SYSTEM_TAG_NAME = "system";
+    public static final String SECTION_TAG_NAME = "section";
+    public static final String GROUP_TAG_NAME = "group";
+    public static final String FIELD_TAG_NAME = "field";
+
     @Override
     public String getFileName() {
         return FILE_NAME;
