26b4206a299b8a9d3e5f8fd074d04aba17b8e723
SilinMykola <m.silin@atwix.com>
2022-06-30 14:49:20 +0300

    1105 add graphQL schema file creation action

--- src/com/magento/idea/magento2plugin/magento/files/GraphQLSchema.java
+++ src/com/magento/idea/magento2plugin/magento/files/GraphQLSchema.java
@@ -2,8 +2,35 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.magento.files;
 
-public class GraphQLSchema {
-    public static String FILE_NAME = "schema.graphqls";
+import com.intellij.lang.Language;
+import com.jetbrains.php.lang.PhpLanguage;
+
+public class GraphQLSchema implements ModuleFileInterface {
+
+    public static final String FILE_NAME = "schema.graphqls";
+
+    public static final String TEMPLATE = "Magento GraphQL Schema";
+    private static final GraphQLSchema INSTANCE = new GraphQLSchema();
+
+    public static GraphQLSchema getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public String getFileName() {
+        return FILE_NAME;
+    }
+
+    @Override
+    public String getTemplate() {
+        return TEMPLATE;
+    }
+
+    @Override
+    public Language getLanguage() {
+        return PhpLanguage.INSTANCE;
+    }
 }
