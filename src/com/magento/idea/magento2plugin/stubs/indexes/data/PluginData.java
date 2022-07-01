c935b5fdb3f80748c2eb504c1e9f1a895a0e42a4
bohdan-harniuk <b.harniuk@atwix.com>
2022-07-01 17:51:16 +0300

    406: Added equality overriddes

--- src/com/magento/idea/magento2plugin/stubs/indexes/data/PluginData.java
+++ src/com/magento/idea/magento2plugin/stubs/indexes/data/PluginData.java
@@ -53,4 +53,35 @@ public class PluginData {
 
         phpClassCollection = phpIndex.getClassesByFQN(getType());
     }
+
+    /**
+     * Overridden hashCode check.
+     *
+     * @return boolean
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.sortOrder;
+
+        return prime * result + ((this.getType() == null) ? 0 : this.getType().hashCode());
+    }
+
+    /**
+     * Overridden quality check.
+     *
+     * @param object PluginData
+     *
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof PluginData)) {
+            return false;
+        }
+        final PluginData compareTo = (PluginData) object;
+
+        return type.equals(compareTo.getType()) && sortOrder == compareTo.getSortOrder();
+    }
 }
