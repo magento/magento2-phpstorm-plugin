e21494d9d3f78a81efa9c1bc56ecf90d0dfa1e92
makzef <m.pletiuk@atwix.com>
2022-07-20 17:01:09 +0300

    1111: Add context dependent action for generation of README.md

---
+++ src/com/magento/idea/magento2plugin/actions/context/md/NewReadmeMdAction.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+package com.magento.idea.magento2plugin.actions.context.md;
+
+import com.intellij.ide.fileTemplates.actions.AttributesDefaults;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.magento.idea.magento2plugin.actions.context.AbstractContextAction;
+import com.magento.idea.magento2plugin.indexes.ModuleIndex;
+import com.magento.idea.magento2plugin.magento.files.ModuleReadmeMdFile;
+import com.magento.idea.magento2plugin.magento.packages.ComponentType;
+import com.magento.idea.magento2plugin.magento.packages.Package;
+import com.magento.idea.magento2plugin.util.magento.GetMagentoModuleUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class NewReadmeMdAction extends AbstractContextAction {
+
+    public static final String ACTION_NAME = "Magento 2 README File";
+    public static final String ACTION_DESCRIPTION = "Create a new Magento 2 README file";
+
+    public NewReadmeMdAction() {
+        super(ACTION_NAME, ACTION_DESCRIPTION, new ModuleReadmeMdFile());
+    }
+
+    @Override
+    protected boolean isVisible(
+            final @NotNull GetMagentoModuleUtil.MagentoModuleData moduleData,
+            final PsiDirectory targetDirectory,
+            final PsiFile targetFile
+    ) {
+        final PsiDirectory moduleDirectory = new ModuleIndex(targetDirectory.getProject())
+                .getModuleDirectoryByModuleName(moduleData.getName());
+        final String magentoModuleName = moduleData.getName().split(Package.vendorModuleNameSeparator)[1];
+
+        return targetDirectory.getName().equals(magentoModuleName)
+                && targetDirectory.equals(moduleDirectory)
+                && moduleData.getType().equals(ComponentType.module);
+    }
+
+    @Override
+    protected AttributesDefaults getProperties(
+            final @NotNull AttributesDefaults defaults,
+            final @NotNull GetMagentoModuleUtil.MagentoModuleData moduleData,
+            final PsiDirectory targetDirectory,
+            final PsiFile targetFile
+    ) {
+        final String[] templateData = moduleData.getName().split(Package.vendorModuleNameSeparator);
+        defaults.addPredefined("PACKAGE", templateData[0]);
+        defaults.addPredefined("MODULE_NAME", templateData[1]);
+
+        return defaults;
+    }
+}
