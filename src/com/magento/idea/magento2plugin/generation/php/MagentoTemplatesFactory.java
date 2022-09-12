d1837a8065ece5e53653ab46898f81256526a547
bohdan-harniuk <b.harniuk@atwix.com>
2022-09-12 16:40:09 +0300

    Fixed new intellij version compatibility issues

--- src/com/magento/idea/magento2plugin/generation/php/MagentoTemplatesFactory.java
+++ src/com/magento/idea/magento2plugin/generation/php/MagentoTemplatesFactory.java
@@ -2,32 +2,35 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 package com.magento.idea.magento2plugin.generation.php;
 
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.platform.ProjectTemplate;
 import com.intellij.platform.ProjectTemplatesFactory;
-import com.jetbrains.php.config.generation.PhpEmptyTemplatesFactory;
+import com.jetbrains.php.config.generation.PhpEmptyProjectGenerator;
 import icons.PhpIcons;
+import javax.swing.Icon;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import javax.swing.Icon;
 
 public class MagentoTemplatesFactory extends ProjectTemplatesFactory {
-    public MagentoTemplatesFactory() {
+
+    @Override
+    public @NotNull String[] getGroups() {
+        return new String[]{PhpEmptyProjectGenerator.PHP_PROJECT_TEMPLATE_GROUP};
     }
 
-    @NotNull
-    public String[] getGroups() {
-        return new String[]{PhpEmptyTemplatesFactory.PHP_PROJECT_TEMPLATE_GROUP};
-    }
-
-    public Icon getGroupIcon(String group) {
+    @Override
+    public Icon getGroupIcon(final String group) {
         return PhpIcons.Php_icon;
     }
 
-    @NotNull
-    public ProjectTemplate[] createTemplates(@Nullable String group, WizardContext context) {
+    @Override
+    public @NotNull ProjectTemplate[] createTemplates(
+            final @Nullable String group,
+            final @NotNull WizardContext context
+    ) {
         return new ProjectTemplate[]{new MagentoModuleGenerator()};
     }
 }
