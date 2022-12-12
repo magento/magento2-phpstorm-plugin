531f0a471245906b9b2f11a46c4d646756177e1a
vitaliy <vitaliyboyko@i.ua>
2022-12-12 21:19:42 +0200

    1239: static fix

--- src/com/magento/idea/magento2uct/versioning/VersionStateManager.java
+++ src/com/magento/idea/magento2uct/versioning/VersionStateManager.java
@@ -227,7 +227,8 @@ public final class VersionStateManager {
      */
     private String escapeFqn(final @NotNull String fqn) {
         return MagentoTypeEscapeUtil.escape(
-                fqn.trim().length() > 0 && fqn.trim().charAt(0) == '\\' ? fqn.trim() : '\\' + fqn.trim()
+                fqn.trim().length() > 0 && fqn.trim().charAt(0)
+                        == '\\' ? fqn.trim() : '\\' + fqn.trim()
         );
     }
 }
