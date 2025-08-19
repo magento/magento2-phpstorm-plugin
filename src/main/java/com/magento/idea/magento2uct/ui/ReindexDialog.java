99dca9335229cc179b91b579dd686bf7f6fc3d1a
Alex Johnson <alex.johnson@boxxe.com>
2025-08-19 19:15:01 +0100

    BUGFIX - Remove buttonOk and buttonCancel references in ReindexDialog to prevent java.lang.NullPointerException

--- src/main/java/com/magento/idea/magento2uct/ui/ReindexDialog.java
+++ src/main/java/com/magento/idea/magento2uct/ui/ReindexDialog.java
@@ -16,7 +16,6 @@ import com.magento.idea.magento2uct.execution.process.ReindexHandler;
 import com.magento.idea.magento2uct.packages.IndexRegistry;
 import com.magento.idea.magento2uct.packages.SupportedVersion;
 import java.awt.event.KeyEvent;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -32,15 +31,13 @@ public class ReindexDialog extends AbstractDialog {
     private JPanel contentPanel;
     private JComboBox<ComboBoxItemData> targetVersion;
     private JComboBox<ComboBoxItemData> targetIndex;
-    private JButton buttonOk;
-    private JButton buttonCancel;
     private JLabel targetVersionLabel;//NOPMD
     private JLabel targetIndexLabel;//NOPMD
 
     /**
      * Reindexing dialog.
      *
-     * @param project Project
+     * @param project   Project
      * @param directory PsiDirectory
      */
     public ReindexDialog(
@@ -53,9 +50,6 @@ public class ReindexDialog extends AbstractDialog {
         this.directory = directory;
 
         setTitle(ReindexVersionedIndexesAction.ACTION_NAME);
-
-        buttonOk.addActionListener(event -> onOK());
-        buttonCancel.addActionListener(event -> onCancel());
 
         // call onCancel() on ESCAPE
         contentPanel.registerKeyboardAction(
@@ -70,7 +64,7 @@ public class ReindexDialog extends AbstractDialog {
     /**
      * Open reindexing dialog window.
      *
-     * @param project Project
+     * @param project   Project
      * @param directory PsiDirectory
      */
     public static void open(
